a003a6910a401d7319d56bc0bad1e59c92f4a05c
Mizuto <mizuto868@gmail.com>
2023-08-30 09:55:00 +0900

    Java1-30

---
+++ Java1-30/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+class Main {
+	public static void main(String[] args) {
+		// 問題5：Main.javaの引数にweightの60を入れてください
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+
+		System.out.println(person1.name);
+		System.out.println(person1.age);
+		System.out.println(person1.height);
+	}
+}
---
+++ Java1-30/src/main/Peroson.java
@@ -0,0 +1,51 @@
+package main;
+
+class Person{
+	/* 問題1：インスタンスフィールドを定義してください
+	 * 問題4：インスタンスフィールド「weight」を定義し、コンストラクタの中で値をセットしてください（double型）
+	 */
+	
+	// インスタンスフィールドを定義
+	public String name;
+	public int age;
+	public double height; 
+	public double weight;
+	public int count;
+	
+	/* 問題2：コンストラクタを定義してください（下記それぞれの引数)
+	 * name,age,height
+	 *
+	 * 問題3：コンストラクタの中で各インスタンスフィールドに値をセットしてください
+	 */
+	
+	// コンストラクタを定義しインスタンスフィールドに値をセット
+	Person(String name, int age, double height, double weight){
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		this.count++;
+	}
+	
+	/* 
+	 * 問題6：インスタンスメソッド「bmi」を定義してください（戻り値：double）
+	 * 問題7：bmiメソッドでインスタンスのBMIを返すようにしてください
+	 */
+	public double bmi() {
+		
+		return weight / (height * height);
+	}
+	
+	/*
+	 * 問題8：インスタンスメソッド「print」を定義してください（戻り値：void）
+	 * 問題9：printメソッドの中でthisを用いて「名前は〇〇です」,「年は〇〇です」「BMIは○○です」と出力してください
+	 * 問題10：人数の合計を「合計○人です」と出力してください。
+	 */
+	public void print() {
+		
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age + "です");
+		System.out.println("BMIは" + this.bmi() + "です");
+		System.out.println("合計"+ this.count +"人です");
+	}
+}
